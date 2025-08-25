342423fdc1158de51fff38d7a24562e500fb461d
Mahendra Kapoor Kol <mahendra.2201242cs@iiitbh.ac.in>
2025-08-25 20:18:30 +0530

    Create Installing Libraries.py

---
+++ Installing Libraries.py
@@ -0,0 +1,10 @@
+# Essential libraries for data handling and visualization
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Tools for preprocessing and deep learning
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow import keras
